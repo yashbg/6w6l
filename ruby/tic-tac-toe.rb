dffe9235a4a595ec076155bf0d1c71e6486d7f1c
Yash Gupta <yashbg7@gmail.com>
2021-06-03 03:39:49 +0530

    Create move function

--- ruby/tic-tac-toe.rb
+++ ruby/tic-tac-toe.rb
@@ -6,10 +6,27 @@ def print_board(board)
     puts board[7] + '|' + board[8] + '|' + board[9]
 end
 
+def move(board, player_X)
+    # will have to handle non integers also
+    print "Choose a position: "
+    pos = gets.chomp.to_i
+    while board[pos] != ' '
+        print "Invalid move. Please choose a different position: "
+        pos = gets.chomp.to_i
+    end
+    board[pos] = player_X ? 'X' : 'O'
+end
+
 board = {
     1 => ' ', 2 => ' ', 3 => ' ',
     4 => ' ', 5 => ' ', 6 => ' ',
     7 => ' ', 8 => ' ', 9 => ' ',
 }
 
-print_board(board)
+player_X = true
+
+while true
+    print_board(board)
+    move(board, player_X)
+    player_X = !player_X
+end
